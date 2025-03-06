006c65572d367becc7193afd64315a06038b7e14
Ada-LXY <13432266188@139.com>
2025-03-06 10:05:08 +0800

    Add files via upload

--- assignment2/Program.cs
+++ assignment2/Program.cs
@@ -1,32 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-
-namespace Task1 {
-	class Program {
-		static List<int> GetFactors(int ob) {
-			List<int> factors = new List<int>();
-			for (int i = 2; i * i <= ob; i++) {
-				if (ob % i == 0) {
-					factors.Add(i);
-					while (ob % i == 0) {
-						ob /= i;
-					}
-				}
-			}
-			if (ob != 1) {
-				factors.Add(ob);
-			}
-			return factors;
-		}
-		static void Main(string[] args) {
-			int ob;
-			bool input = int.TryParse(Console.ReadLine(), out ob);
-			if (input == false) {
-				Console.WriteLine("Error Input");
-				return;
-			}
-			var factors = GetFactors(ob);	
-			Console.WriteLine(string.Join(" ", factors));
-		}
-	}
+using System.IO.Pipes;
+class assignment2_3
+{
+    static bool[] exit = new bool[101];
+    static int num = 0;
+    static int[] ans = new int[101];
+    static int[] findsu()
+        {
+            for (int i = 2; i <= 100; ++i)
+            {
+            
+                if (exit[i] == false)
+                {
+                    //Console.WriteLine(ans[num]);
+                    ans[++num] = i;
+                    for (int times = 1; i * times <= 100; times += 1)
+                    {
+                        exit[i * times] = true;
+                    }
+                }
+            }
+        return ans;
+        }
+    static int Main()
+    {
+        int[] ans = findsu();
+        for (int i = 1;i <= num; i++)
+        {
+            Console.WriteLine(ans[i]);
+        }
+        return 0;
+    }
 }
